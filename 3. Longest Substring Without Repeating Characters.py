0790b234e2c12a8bd4221499bc4408ab3dcbbf82
criszhou <crision_cn@hotmail.com>
2016-07-30 21:39:45 -0400

    minor change

--- 3. Longest Substring Without Repeating Characters.py
+++ 3. Longest Substring Without Repeating Characters.py
@@ -6,12 +6,11 @@ class Solution(object):
         currWindowBegin = 0  # starting at this index, we don't have any repeated char
 
         for i, c in enumerate(s):
-
             if c in charToLastIndex and charToLastIndex[c] >= currWindowBegin:
                 currWindowBegin = charToLastIndex[c] + 1
 
             ret = max(ret, i + 1 - currWindowBegin)
-
+            
             charToLastIndex[c] = i
 
         return ret
